c79792b3aa4a1c24e01d44ff4c7e2c7815579ccd
Nassim <nassim.amokrane@hotmail.com>
2021-06-05 18:13:58 +0100

    🎉 WuuD team icon to main screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+import wuud from "./assets/Group 3.svg";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: azure;
+`;
+
+const Logo = styled.img`
+  width: 40%;
+`;
 
 function App() {
-  return <div>Work in progress</div>;
+  return (
+    <Container>
+      <Logo src={wuud} />
+    </Container>
+  );
 }
 
 export default App;
