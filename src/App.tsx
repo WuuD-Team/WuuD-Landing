450416ea3d8ef44b206aa227452bbad7254a9881
Med Redha <redha.dirac@gmail.com>
2022-07-08 04:53:46 +0200

    ✨  update temporary landing page

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,28 @@ const Container = styled.div`
   height: 100vh;
   width: 100vw;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: #1c1c1e;
+  background-color: #1c1f26;
 `;
 
 const Logo = styled.img`
-  width: 40%;
+  width: 100%;
+`;
+
+const Copyright = styled.p`
+  bottom: 10%;
+  color: white;
+  position: absolute;
+  text-align: center;
+  font-family: "Urbanist";
+  @media (min-width: 768px) {
+    font-size: 18px;
+  }
+  @media (max-width: 768px) {
+    font-size: 15px;
+  }
 `;
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
@@ -20,6 +35,7 @@ function App() {
   return (
     <Container>
       <Logo src={wuud} />
+      <Copyright>© 2022 WuuD Team Technologies, Inc.</Copyright>
     </Container>
   );
 }
